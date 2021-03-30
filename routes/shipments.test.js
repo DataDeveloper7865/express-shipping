79470009d5243d5c36b5745d52220e5ac6e47909
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-03-30 15:52:50 -0700

    added json schema

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -15,4 +15,16 @@ describe("POST /", function () {
 
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
   });
+
+  test("invalid Input", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 100000000,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-6789",
+    });
+
+    expect(resp.body.error.message[0]).toEqual("instance.productId must be less than or equal to 1000");
+  });
+
 });
